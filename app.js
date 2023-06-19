1c4a343e3d968745072b6709935aa65d5762589e
Thiago Diógenes Maia Gonçalves <thiago.maia@dotzmkt.com.br>
2023-06-19 19:42:11 -0300

    feat(Auth JWT): Jwt Verify, token and middleware

--- app.js
+++ app.js
@@ -14,9 +14,43 @@ app.use(express.json());
 
 const User = require("./models/User");
 
+// Open Route - Public Route
+
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo a nossa API!" });
 });
+
+// Private Route
+
+app.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  // check if user exists
+  const user = await User.findById(id, "-password");
+
+  if (!user) {
+    return res.status(422).json({ msg: "Usuário não encontrado!" });
+  }
+
+  res.status(200).json({ user });
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ msg: "Acesso negado!" });
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "O Token é inválido!" });
+  }
+}
 
 // Register User
 
