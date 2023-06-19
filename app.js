04fbaadb492ca42c7d57da7c7bb725738efd1a81
Thiago Diógenes Maia Gonçalves <thiago.maia@dotzmkt.com.br>
2023-06-19 17:29:26 -0300

    feat(Auth JWT): Create database and Env Variables

--- app.js
+++ app.js
@@ -10,4 +10,13 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo a nossa API!"})
 })
 
-app.listen(3000)
+// Credencials
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.n066ec1.mongodb.net/?retryWrites=true&w=majority`).then(()=> {
+    app.listen(3000)
+    console.log('Conectado')
+}).catch((err) => console.log(err))
+
